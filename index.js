a2ef93133ea8f29cbec3315a4e4bb4ea1085fb9b
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-11-28 16:19:45 +0100

    kacke

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const bookmarkButtons = document.querySelectorAll(".bookmark");
+const antwortButton = document.getElementById("antwortButton");
+const verbergenButton = document.getElementById("verbergenButton");
+const antwortTextElement = document.getElementById("antwort");
 
 bookmarkButtons.forEach((button) => {
   button.addEventListener("click", () => {
     button.classList.toggle("button-toggle");
   });
+});
+
+antwortButton.addEventListener("click", function () {
+  antwortTextElement.textContent = "🤯";
+  antwortTextElement.style.display = "block";
+  antwortButton.style.display = "none";
+  verbergenButton.style.display = "block";
+});
+
+verbergenButton.addEventListener("click", function () {
+  antwortTextElement.style.display = "none";
+  antwortButton.style.display = "block";
+  verbergenButton.style.display = "none";
 });
